b55f286ec5d7f4fd72de0a5dd390857d11f64dce
Khurmatbek <xurmatbekolloyorov1514@gmail.com>
2023-08-17 10:48:25 +0500

    initial commit

--- main.js
+++ main.js
@@ -3,11 +3,14 @@ const elSearch = document.querySelector(".search");
 const elsection = document.querySelector(".title-image");
 const list = document.querySelector(".list");
 const div = document.querySelector(".error-box");
-const text=document.querySelector(".error");
+const text = document.querySelector(".error");
+const btn = document.querySelector(".btn");
+const pokename = document.querySelector(".modal-title");
+const modalyear = document.querySelector(".modal__year")
+const modaimage = document.querySelector(".modal__img");
 const pokemonsFunction = function (searchelemet) {
     list.innerHTML = " ";
-    console.log(searchelemet);
-    searchelemet == [] ? alert("bosh") : searchelemet.forEach(element => {
+    searchelemet.forEach(element => {
         /*  create item  */
         let item = document.createElement("li");
         item.classList.add("item")
@@ -44,12 +47,19 @@ const pokemonsFunction = function (searchelemet) {
         let descheight = document.createElement("p");
         let descspawntime = document.createElement("time");
         let descname = document.createElement("p");
+        let modalbtn = document.createElement("button");
+        modalbtn.classList.add("btn");
         /* ******************************************************* */
         descweight.classList.add("desc");
         desceggs.classList.add("desc");
         descheight.classList.add("desc");
         descspawntime.classList.add("desc");
         descname.classList.add("name");
+        modalbtn.classList.add("btn", "btn-primary");
+        modalbtn.setAttribute("data-bs-toggle", "modal")
+        modalbtn.setAttribute("data-bs-target", "#exampleModal");
+        modalbtn.textContent = "more.."
+        modalbtn.dataset.id = element.id;
         /****************************************************** */
         item.appendChild(listSec);
         listSec.appendChild(itemweight);
@@ -57,6 +67,7 @@ const pokemonsFunction = function (searchelemet) {
         listSec.appendChild(itemeggs);
         listSec.appendChild(itemspawntime);
         listSec.appendChild(itemname);
+        item.appendChild(modalbtn);
         /* *********************************************** */
         itemweight.appendChild(descweight);
         itemeggs.appendChild(desceggs);
@@ -73,17 +84,35 @@ const pokemonsFunction = function (searchelemet) {
     })
 }
 pokemonsFunction(pokemons);
-
 elForm.addEventListener("submit", function (evt) {
     evt.preventDefault();
     const searchvalue = elSearch.value.trim().toLowerCase();
     let newsearch = pokemons.filter(function (element) {
-       
-        return  element.name.toLowerCase().includes(searchvalue);
+        return element.name.toLowerCase().includes(searchvalue);
     })
-    pokemonsFunction(newsearch); 
- 
+    pokemonsFunction(newsearch);
+
 })
+
+list.addEventListener("click", (evt) => {
+    if (evt.target.matches(".btn")) {
+        const btnId = evt.target.dataset.id;
+        console.log(btnId);
+        const finded = pokemons.find(element => {
+            return element.id== btnId;
+        });
+        pokename.textContent = finded.name;
+        modalyear.textContent = finded.weaknesses;
+        modaimage.setAttribute("src", `${finded.img}`)
+    }
+
+})
+
+// if (evt.target.matches(".btn-remove")) {
+//     const deletId = evt.target.dataset.id;
+//     const findobj = array.findIndex(item => {
+//         return item.id = deletId;
+//     });
 
 
 
